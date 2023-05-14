077608e9dae90d10ba09f1e9444c17428bbaf8eb
Nikhil Sonti <nikhilsv92@gmail.com>
2023-05-14 22:01:22 +0100

    first commit. v0.2 release

---
+++ README.md
@@ -0,0 +1,24 @@
+# BardGPT for desktop
+
+This is a simple app that makes BardGPT live in your menubar.
+
+You can use Cmd+Shift+G (Mac) or Ctrl+Shift+G (Win) to quickly open it from anywhere.
+
+Download:
+
+- Mac:  Apple Silicon/Arm64 Installer - [dmg](https://github.com/shadowfax92/BardGPT/releases/download/release-0.0.2/BardGPT-0.0.2-arm64.dmg)
+- Mac: Intel Installer - [dmg](https://github.com/shadowfax92/BardGPT/releases/download/release-0.0.2/BardGPT-0.0.2-x64.dmg)
+
+No Windows binaries currently offered. Clone the repo, npm install electron-forge and run.
+
+<p align="center">
+  <img src="./images/screenshot.png" width="500">
+</p>
+
+## Credit
+- Huge thanks to [@vincelwt](https://github.com/vincelwt) for [chatgpt-mac](https://github.com/vincelwt/chatgpt-mac) based on which this is built.
+- All credit and copyrights goes to Google.
+
+## Author
+
+You can find me on Twitter [@ShadowfaxApp](https://twitter.com/ShadowfaxApp).
---
+++ forge.config.js
@@ -0,0 +1,65 @@
+const { parsed } = require("dotenv").config();
+module.exports = {
+  packagerConfig: {
+    name: "BardGPT",
+    executableName: "BardGPT",
+    icon: "images/icon",
+    appBundleId: "com.shadowfax.bardgpt",
+    extendInfo: {
+      LSUIElement: "true",
+    },
+    osxSign: {
+      hardenedRuntime: false,
+      gatekeeperAssess: false,
+      identity: "Developer ID Application: Nikhil Venkat Sonti (8YMKWU47S5)",
+    },
+    osxNotarize: {
+      appBundleId: "com.shadowfax.bardgpt",
+
+      tool: "notarytool",
+      appleId: parsed.APPLE_ID,
+      appleIdPassword: parsed.APPLE_PASSWORD,
+      teamId: parsed.APPLE_TEAM_ID,
+    },
+  },
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "shadowfax92",
+          name: "BardGPT-Mac",
+        },
+        prerelease: true,
+      },
+    },
+  ],
+
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      platforms: ["darwin"],
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["win32"],
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      platforms: ["linux"],
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      platforms: ["linux"],
+      config: {},
+    },
+  ],
+};
